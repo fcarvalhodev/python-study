c8a9f0b011ff610e0cea996f0d551a847141b342
Fabio Carvalho <fabio.carvalllho@live.com>
2023-02-04 23:47:26 -0300

    include binary-search, select-search and recursive algorithms with explanation

---
+++ chapter-2/select-search.py
@@ -0,0 +1,22 @@
+def smallSearch(arr):
+    minor = arr[0]
+    minor_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < minor:
+            minor = arr[i]
+            minor_index = i
+    return minor_index
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        minor = smallSearch(arr)
+        newArr.append(arr.pop(minor))
+    return newArr
+
+print(selectionSort([5, 3, 6, 2, 10]))
+
+# The Big O notation for selection sort is O(n^2).
+# This is because there are two nested loops.
+# The first loop goes through n elements.
+# The second loop also goes through n elements for each iteration of the first loop.
---
+++ chapter-3/recursive-box.py
@@ -0,0 +1,34 @@
+class Item:
+    def is_a_key(self):
+        return False
+
+class Box(Item):
+    def __init__(self, items):
+        self.items = items
+
+    def __iter__(self):
+        return iter(self.items)
+
+class Key(Item):
+    def is_a_key(self):
+        return True
+
+
+def search_for_the_key(item):
+    if isinstance(item, Key) and item.is_a_key():
+        print("Found the key!")
+        return
+    elif isinstance(item, Box):
+        for sub_item in item:
+            search_for_the_key(sub_item)
+    else:
+        print("Item is not a key or a box.")
+
+items = [1, 2, 3, Key(), 4, 5, 6, Box([7, 8, 9, Key(), 10])]
+box = Box(items)
+search_for_the_key(box)
+
+#The code is a recursive function that searches for an object with the "is_a_key" method
+#inside a box object. The box object must contain other objects that can either be boxes or keys.
+#Time complexity is O(n) as the function needs to traverse all elements in the worst-case scenario.
+#Space complexity is O(n) as well, as it needs to store the calls to the function in the call stack.
